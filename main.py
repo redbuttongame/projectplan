292867f17cd810103d2844f1a2ea45489e875cff
aadritnandy <aadritnandy10@gmail.com>
2025-03-06 12:34:19 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from app import app
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Check if running in production environment
+    if os.environ.get('FLASK_ENV') == 'production':
+        app.run(host="0.0.0.0", port=5000, ssl_context='adhoc')
+    else:
+        app.run(host="0.0.0.0", port=5000, debug=True)
